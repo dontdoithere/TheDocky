f64a3535fee283d0b15bc4bac7fc634a59633add
dontdoithere <seeema34l@gmail.com>
2022-09-25 16:21:28 -0400

    First Commit to GitHub. Just created project.

---
+++ app/src/main/java/ca/group6/it/thedocky/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package ca.group6.it.thedocky.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
