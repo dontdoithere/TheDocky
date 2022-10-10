40851ab40122b4213b66ad3ac95fe0c0cddc9f39
binaygarlapati8870 <gbinaypavan24@gmail.com>
2022-10-09 22:33:23 -0400

    I have added splash screen activity to the app and tested it with styling it.

---
+++ app/src/main/java/ca/group6/it/thedocky/SplashScrActivity.java
@@ -0,0 +1,28 @@
+package ca.group6.it.thedocky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_scr);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScrActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
