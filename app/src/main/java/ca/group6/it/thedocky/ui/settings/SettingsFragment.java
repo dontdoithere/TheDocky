111c744021de459561a60e669a93ea08ddf785ca
nissan <r.nissan2010@gmail.com>
2022-10-10 17:22:53 -0400

    Created settings fragment and linked it to the main activity.

--- app/src/main/java/ca/group6/it/thedocky/ui/settings/SettingsFragment.java
+++ app/src/main/java/ca/group6/it/thedocky/ui/settings/SettingsFragment.java
@@ -1,15 +1,37 @@
+//Semen Dyakonov. N01391812
+//Binay Garlapati. N01368870
+//Nissan Rayappu N01435235
+//
 package ca.group6.it.thedocky.ui.settings;
 
+import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import ca.group6.it.thedocky.databinding.FragmentSettingsBinding;
 
-import androidx.preference.PreferenceFragmentCompat;
+public class SettingsFragment extends Fragment {
+    private FragmentSettingsBinding binding;
 
-import ca.group6.it.thedocky.R;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SettingsViewModel settingsViewModel =
+                new ViewModelProvider(this).get(SettingsViewModel.class);
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+
+        return root;
+    }
 
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
