997ac523347bd75bc26194ca0913663df70290ba
nissan <r.nissan2010@gmail.com>
2022-10-13 15:39:47 -0400

    setting screen done

--- app/src/main/java/ca/group6/it/thedocky/ui/settings/SettingsFragment.java
+++ app/src/main/java/ca/group6/it/thedocky/ui/settings/SettingsFragment.java
@@ -4,13 +4,24 @@
 //
 package ca.group6.it.thedocky.ui.settings;
 
+import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.ViewModelProvider;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import ca.group6.it.thedocky.SplashScrActivity;
 import ca.group6.it.thedocky.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
@@ -25,6 +36,61 @@ public class SettingsFragment extends Fragment {
         View root = binding.getRoot();
 
 
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+            binding.switchCamera.setChecked(true);
+
+        }
+
+        binding.changeOrientation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if (b) {
+
+                    getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+                } else {
+
+                    getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+                }
+
+            }
+        });
+        binding.switchCamera.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if (b) {
+
+                    if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+
+                        ActivityCompat.requestPermissions(getActivity(),
+                                new String[]{Manifest.permission.CAMERA}, 2);
+
+                    }else {
+                        Snackbar.make(binding.getRoot(), "Persimmon already granted", Snackbar.LENGTH_LONG).show();
+                    }
+
+                } else {
+
+
+                }
+
+            }
+        });
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity(), SplashScrActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+
+            }
+        });
 
         return root;
     }
