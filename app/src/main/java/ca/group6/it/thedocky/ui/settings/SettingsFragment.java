92aa35a903c355b2f86700043b2d3cab7fca0ca6
binaygarlapati8870 <gbinaypavan24@gmail.com>
2022-11-03 23:09:48 -0400

    Added some more design and UX features for settings fragment.

--- app/src/main/java/ca/group6/it/thedocky/ui/settings/SettingsFragment.java
+++ app/src/main/java/ca/group6/it/thedocky/ui/settings/SettingsFragment.java
@@ -4,27 +4,38 @@
 //
 package ca.group6.it.thedocky.ui.settings;
 
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import android.os.Handler;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import ca.group6.it.thedocky.R;
 import ca.group6.it.thedocky.SplashScrActivity;
 import ca.group6.it.thedocky.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
+
+    View view;
     private FragmentSettingsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -34,7 +45,22 @@ public class SettingsFragment extends Fragment {
 
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        binding.b.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
 
+                if (b) {
 
+                    getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+                } else {
+
+                    getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+                }
+
+            }
+        });
         return root;
     }}
+
