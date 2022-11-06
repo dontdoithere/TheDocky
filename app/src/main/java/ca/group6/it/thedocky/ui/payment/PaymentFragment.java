d528a9293216a07b47fa8f0df3b734b84498b1cc
nissan <r.nissan2010@gmail.com>
2022-11-06 12:31:11 -0500

    Designed the payment method screen successfully and tested it

--- app/src/main/java/ca/group6/it/thedocky/ui/payment/PaymentFragment.java
+++ app/src/main/java/ca/group6/it/thedocky/ui/payment/PaymentFragment.java
@@ -4,38 +4,43 @@
 //
 package ca.group6.it.thedocky.ui.payment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import ca.group6.it.thedocky.R;
 import ca.group6.it.thedocky.databinding.FragmentPaymentBinding;
 
 public class PaymentFragment extends Fragment {
-
-    private FragmentPaymentBinding binding;
-
+    private static final String TAG = "PaymentFragment";
+    
+    private CardView cardView;  
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        PaymentViewModel paymentViewModel =
-                new ViewModelProvider(this).get(PaymentViewModel.class);
 
-        binding = FragmentPaymentBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View view=inflater.inflate(R.layout.fragment_payment,container,false);
+        cardView = view.findViewById(R.id.card_pay_id);
+        
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Click");
+                startActivity(new Intent(getContext(),PaymentActivity.class));
+            }
+        });        
 
-
-
-        return root;
+        return view;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
+  
 }
