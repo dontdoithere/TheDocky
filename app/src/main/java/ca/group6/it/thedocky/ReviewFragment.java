8780e5b2a81aa0dfc7f3273b26b7413b2016e4ae
nissan <r.nissan2010@gmail.com>
2022-11-15 11:50:36 -0500

    Linked Review to our database

--- app/src/main/java/ca/group6/it/thedocky/ReviewFragment.java
+++ app/src/main/java/ca/group6/it/thedocky/ReviewFragment.java
@@ -9,7 +9,15 @@ import androidx.lifecycle.ViewModelProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RatingBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import ca.group6.it.thedocky.databinding.FragmentReviewBinding;
 
@@ -17,8 +25,9 @@ import ca.group6.it.thedocky.databinding.FragmentReviewBinding;
 
 public class ReviewFragment extends Fragment {
 
+    DatabaseReference databaseReference;
 
-
+    private FirebaseAuth auth;
 
     private FragmentReviewBinding binding;
 
@@ -27,18 +36,48 @@ public class ReviewFragment extends Fragment {
         ReviewViewModel ReviewViewModel =
                 new ViewModelProvider(this).get(ReviewViewModel.class);
 
+        databaseReference = FirebaseDatabase.getInstance().getReference("Review");//path
+
+        ReviewViewModel reviewViewModel = new ReviewViewModel();
+
+        String uid=databaseReference.push().getKey();
+
+        reviewViewModel.setQid("uid");
+        reviewViewModel.setName("Enter the user name");
+        reviewViewModel.setAddress("Enter the address");
+        reviewViewModel.setEmail("Enter the email address");
+        reviewViewModel.setPhonenumber("Enter the phone number");
+        reviewViewModel.setFeedback("Enter the feedback");
+
+
+
+
+        databaseReference.child(uid).setValue(reviewViewModel).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Toast.makeText(getActivity(), "Success", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(), "Failure", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
         binding = FragmentReviewBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-}
 
+        @Override
+        public void onDestroyView () {
+            super.onDestroyView();
+            binding = null;
+        }
+
+    }
 
 
--- app/src/main/java/ca/group6/it/thedocky/ReviewViewModel.java
+++ app/src/main/java/ca/group6/it/thedocky/ReviewViewModel.java
@@ -1,19 +1,95 @@
 package ca.group6.it.thedocky;
 
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ReviewViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
 
-    public ReviewViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is Review fragment");
+    private String Qid;
+    private String name;
+    private String phonenumber;
+    private String email;
+    private String address;
+    private String feedback;
+
+    private FirebaseAuth auth;
+
+
+    public ReviewViewModel(String qid, String name, String phonenumber, String email, String address, String feedback, MutableLiveData<String> mText) {
+        Qid = qid;
+        this.name = name;
+        this.phonenumber = phonenumber;
+        this.email = email;
+        this.address = address;
+        this.feedback = feedback;
+
+
     }
 
-    public LiveData<String> getText() {
-        return mText;
+
+
+    public ReviewViewModel() {
+
+    }
+
+    public String getQid(){
+        return Qid;
+    }
+
+    public void setQid(String qid){
+        Qid= qid;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+
+
+    public FirebaseAuth getAuth() {
+        return auth;
+    }
+
+    public void setAuth(FirebaseAuth auth) {
+        this.auth = auth;
     }
 }
