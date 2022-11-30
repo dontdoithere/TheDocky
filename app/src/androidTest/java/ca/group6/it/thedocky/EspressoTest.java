ba9c8c5dcf8b0fa43bef12749f40feb1cc858ac3
binaygarlapati8870 <gbinaypavan24@gmail.com>
2022-11-29 22:15:49 -0500

    Test case written to check the forgot password is displayed on login activity. Test was Successful.

--- app/src/androidTest/java/ca/group6/it/thedocky/EspressoTest.java
+++ app/src/androidTest/java/ca/group6/it/thedocky/EspressoTest.java
@@ -1,5 +1,12 @@
 package ca.group6.it.thedocky;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.PositionAssertions.isCompletelyBelow;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -8,18 +15,22 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import ca.group6.it.thedocky.ui.login.LoginActivity;
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 
 
 public class EspressoTest {
     @Rule
-    public ActivityScenarioRule<MainActivity> activityRule =
-            new ActivityScenarioRule<MainActivity>(MainActivity.class);
+    public ActivityScenarioRule<LoginActivity> activityRule =
+            new ActivityScenarioRule<LoginActivity>(LoginActivity.class);
 
     @Test
-    public void signupIsDisplayedBelowLoginBox()
-    {
+    public void forgotpasswordIsDisplayedBelowPassword() {
+        onView(withText("Forgot password")).check(matches(isDisplayed()));
+        onView(withText("Forgot password")).check(isCompletelyBelow(withId(R.id.password_login)));
 
     }
 }
+
