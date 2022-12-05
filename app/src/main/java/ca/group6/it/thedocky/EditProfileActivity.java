2c03a37de115e9695c13d0808e978e2a9c1da025
dontdoithere <seeema34l@gmail.com>
2022-12-05 17:34:46 -0500

    Start to work on Edit profile activity

---
+++ app/src/main/java/ca/group6/it/thedocky/EditProfileActivity.java
@@ -0,0 +1,63 @@
+package ca.group6.it.thedocky;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    TextInputEditText username;
+    TextInputEditText email;
+    TextInputEditText password;
+
+    AppCompatButton update;
+
+    //Global Variables
+    String _USERNAME, _EMAIL;
+
+    DatabaseReference reference;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+
+        //Database
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+
+        //Initialize text and button
+        username = findViewById(R.id.editProfUsername);
+        email = findViewById(R.id.editProfEmail);
+        password = findViewById(R.id.editProfPass);
+        update = findViewById(R.id.update_button);
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(this,"In progress...", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    public void updateInfo(View view){
+        if(isNameChanged() || isPasswordChanged ()){
+            Toast.makeText(this, "Data has been updated!", Toast.LENGTH_SHORT).show();
+            
+        }
+    }
+
+    private boolean isPasswordChanged() {
+
+    }
+
+    private boolean isNameChanged() {
+    }
+}
